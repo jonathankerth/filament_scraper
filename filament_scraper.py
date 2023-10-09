d88b305efee93abd5271d8b5dc3bdcd50a5e1c8d
jonathankerth <jonathanpkerth@gmail.com>
2023-10-09 11:11:23 -0700

    prep for heroku hosting

--- filament_scraper.py
+++ filament_scraper.py
@@ -1,9 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 import json
 
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
 def fetch_filament_data():
-    url = "https://www.esun3d.com/pla-pro-product/"  # Replace with the actual URL
+    url = "https://www.esun3d.com/pla-pro-product/"
     response = requests.get(url)
     if response.status_code != 200:
         return None
@@ -25,8 +30,14 @@ def fetch_filament_data():
         "net_weight": net_weight
     })
 
-    with open("filament_data.json", "w") as f:
-        json.dump(filaments, f)
+    return filaments
+
+@app.route('/api/filamentData', methods=['GET'])
+def get_filament_data():
+    data = fetch_filament_data()
+    if not data:
+        return jsonify({"error": "Unable to fetch filament data"}), 500
+    return jsonify(data)
 
 if __name__ == "__main__":
-    fetch_filament_data()
+    app.run(port=5000)  # This will start the Flask app on port 5000
